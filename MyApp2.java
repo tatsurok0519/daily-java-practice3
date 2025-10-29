3df04a7db6440911ae6a3780eef10ba14e48d58b
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-30 04:17:02 +0900

    2

---
+++ MyApp2.java
@@ -0,0 +1,10 @@
+public class MyApp2 {
+  private static int triple(int num) {
+    return num * 3;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(triple(10));
+    System.out.println(triple(4) * 3);
+  }
+}
