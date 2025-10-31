72dadb853f0e4b77996e5868464bbc8c32c88751
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-01 04:03:06 +0900

    17

---
+++ MyApp17.java
@@ -0,0 +1,40 @@
+class User {
+  String name;
+  private int score;
+
+  User(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  String getUserString() {
+    return this.name + ", " + this.score;
+  }
+
+  void setScore(int score) {
+    if (score < 0 || score > 100) {
+      System.out.println("Invalid score!");
+      return;
+    }
+    this.score = score;
+  }
+
+  int getScore() {
+    return this.score;
+  }
+}
+
+public class MyApp17 {
+  public static void main(String[] args) {
+    User user1 = new User("Taro", 70);
+    User user2 = new User("Jiro", 80);
+
+    // user1.score = 900;
+    // user1.setScore(900);
+
+    System.out.println(user1.getScore());
+
+    System.out.println(user1.getUserString());
+    System.out.println(user2.getUserString());
+  }
+}
